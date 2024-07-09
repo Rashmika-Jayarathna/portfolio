2f9a9aa17514584c8522af275177624684314ffb
Rashmika Jayarathna <it22311986@my.sliit.lk>
2024-07-09 21:20:28 +0530

    Added nav bar frontend to both mobile and desktop views

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,25 @@
-import React from 'react'
+import MobileNav from '@/components/MobileNav'
+import Nav from '@/components/Nav'
+import React, { useState } from 'react'
 
 const HomePage = () => {
+
+  const [nav, setNav] = useState(false)
+  const openNav = () => setNav(true)
+  const closeNav = () => setNav(false)
+
+
+
   return (
-    <div>HomePage</div>
+    <div className='overflow-x-hidden'>
+      <div>
+        {/* Nav bar */}
+        <MobileNav nav = {nav} closeNav={closeNav}/>
+        <Nav openNav={openNav}/>
+        
+        {/* Hero section */}
+      </div>
+    </div>
   )
 }
 
