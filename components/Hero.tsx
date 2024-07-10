1ec1026630d2004848a68ca5d805b0fe75891486
Rashmika Jayarathna <it22311986@my.sliit.lk>
2024-07-10 19:00:31 +0530

    bug 2 fixed

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -6,7 +6,7 @@ import { ArrowDownTrayIcon } from "@heroicons/react/20/solid";
 
 const Hero = () => {
   return (
-    <div className="h-[88vh] bg-[url('./images/banner.jpg')] mt-[10vh] bg-cover bg-center ">
+    <div className="h-[88vh] bg-[url('/images/banner.jpg')] mt-[10vh] bg-cover bg-center ">
       <Particle />
       <div className="w-[80%] grid-cols-1 mx-auto grid lg:grid-cols-2 gap-[3rem] h-[100%] items-center ">
         <div>
