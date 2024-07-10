3ef5f685c80f1299d19841635b9bc10b998a8b55
Rashmika Jayarathna <it22311986@my.sliit.lk>
2024-07-10 20:44:50 +0530

    Changed download mechanism

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -5,6 +5,16 @@ import Image from "next/image";
 import { ArrowDownTrayIcon } from "@heroicons/react/20/solid";
 
 const Hero = () => {
+  const handleDownload = () => {
+    const url = "/Jayarathana-g-w-n-r.pdf";
+    const fileName = "Rashmika_Jayarathana_CV.pdf";
+    const link = document.createElement("a");
+    link.href = url;
+    link.setAttribute("download", fileName);
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+  };
   return (
     <div className="h-[88vh] bg-[#0b0817] mt-[10vh] bg-cover bg-center ">
       <Particle />
@@ -21,12 +31,12 @@ const Hero = () => {
             technologies and contributing to innovative projects.
           </p>
           <div className="mt-[2rem] flex-col space-y-6 sm:space-y-0 sm:flex sm:flex-row items-center sm:space-x-6">
-            <a href="/Jayarathana-g-w-n-r.pdf" download>
-              <button className="flex px-[2rem] hover:bg-yellow-400 transition-all duration-200 py-[1rem] text-[18px] font-bold uppercase bg-[#55e6a5] text-black items-center space-x-2 ">
+            {/* <a href="/Jayarathana-g-w-n-r.pdf" download> */}
+              <button onClick={handleDownload} className="flex px-[2rem] hover:bg-yellow-400 transition-all duration-200 py-[1rem] text-[18px] font-bold uppercase bg-[#55e6a5] text-black items-center space-x-2 ">
                 <p>Download CV</p>
                 <ArrowDownTrayIcon className="w-[1.6rem] h-[1.7rem] text-black" />
               </button>
-            </a>
+            {/* </a> */}
           </div>
         </div>
 
