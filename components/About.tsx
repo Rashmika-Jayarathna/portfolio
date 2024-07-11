38411e390fb1c52f69531576c8855924067a3199
Rashmika Jayarathna <it22311986@my.sliit.lk>
2024-07-11 09:49:12 +0530

    Fixed download spamming Issue

--- components/About.tsx
+++ components/About.tsx
@@ -37,12 +37,12 @@ const About = () => {
               a difference.
             </p>
           </div>
-          <a href="/Jayarathana-g-w-n-r.pdf" download>
+          
             <button onClick={handleDownload}  className="flex px-[2rem] hover:bg-yellow-400 transition-all duration-200 py-[1rem] text-[18px] font-bold uppercase bg-[#55e6a5] text-black items-center space-x-2 ">
               <p>Download CV</p>
               <ArrowDownTrayIcon className="w-[1.6rem] h-[1.7rem] text-black" />
             </button>
-          </a>
+          
         </div>
         <div
           data-aos="fade-left"
